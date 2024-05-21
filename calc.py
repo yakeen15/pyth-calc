e87425bf55a27273685a4d7cae351a5387f7909d
yakeen15 <shajiratulyakeen@gmail.com>
2024-05-21 21:48:04 +0600

    Added addition evaluation, need to handle adjacent operators

--- calc.py
+++ calc.py
@@ -1,1 +1,59 @@
-#start coding
+def op_adjacent(string):
+    slist = list(string)
+    addops = ['+','-']
+    for i in range(len(string)-1):
+        if slist[i] in addops and slist[i+1] in addops:
+            if slist[i]==slist[i+1]:
+                slist[i] = '+'
+            else:
+                slist[i] = '-'
+            del slist[i+1]
+            i = i-1
+    new_string = ''.join(slist)
+    return new_string
+
+
+def separate_expression(string):
+    ops = ['+','-']
+    oplist = []
+    numlist = []
+    lastpos = 0
+    for index, char in enumerate(string):
+        if char in ops:
+            oplist.append(char)
+            numlist.append(string[lastpos:index])
+            lastpos = index+1
+    numlist.append(string[lastpos:len(string)])
+    return oplist, numlist
+
+def eval_expr(ops, nums):
+    n = len(nums)
+    negcount = 0
+    result = 0
+    for _ in range(n-1):
+        num1, num2 = float(nums[0]), float(nums[1])
+        op = ops[0]
+        if op=='+':
+            result = num1 + num2
+        else:
+            if num1<num2:
+                negcount = negcount + 1
+                if len(ops)>1:
+                    ops[1] = '+' if ops[1] == '-' else '-'
+            result = abs(num1-num2)
+        del ops[0]
+        if len(nums)>1:
+            del nums[0]
+            nums[0] = str(result)
+    return (-1)**negcount*(float(nums[0]))
+
+inputstring = input("Enter expression: ")
+mainstring = op_adjacent(inputstring)
+print(mainstring)
+#for i in range(100):
+#    inputstring = input("Enter expression: ")
+#    if inputstring == 'x':
+#        break
+#    mainstring = '0'+inputstring
+#    ops, nums = separate_expression(mainstring)
+#    print(eval_expr(ops, nums))
