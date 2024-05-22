ca5cebb8c20a0ce1974f0dfa9145cd7fcc8af433
yakeen15 <shajiratulyakeen@gmail.com>
2024-05-22 20:06:20 +0600

    fixed op_adjacent

--- calc.py
+++ calc.py
@@ -1,14 +1,16 @@
 def op_adjacent(string):
     slist = list(string)
     addops = ['+','-']
-    for i in range(len(string)-1):
+    i = 0
+    while i < len(slist):
         if slist[i] in addops and slist[i+1] in addops:
             if slist[i]==slist[i+1]:
                 slist[i] = '+'
             else:
                 slist[i] = '-'
             del slist[i+1]
-            i = i-1
+            i = 0
+        i = i+1
     new_string = ''.join(slist)
     return new_string
 
@@ -47,13 +49,10 @@ def eval_expr(ops, nums):
             nums[0] = str(result)
     return (-1)**negcount*(float(nums[0]))
 
-inputstring = input("Enter expression: ")
-mainstring = op_adjacent(inputstring)
-print(mainstring)
-#for i in range(100):
-#    inputstring = input("Enter expression: ")
-#    if inputstring == 'x':
-#        break
-#    mainstring = '0'+inputstring
-#    ops, nums = separate_expression(mainstring)
-#    print(eval_expr(ops, nums))
+for i in range(100):
+    inputstring = input("Enter expression: ")
+    if inputstring == 'x':
+        break
+    mainstring = '0'+op_adjacent(inputstring)
+    ops, nums = separate_expression(mainstring)
+    print(eval_expr(ops, nums))
