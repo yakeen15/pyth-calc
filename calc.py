a4338fbda69c7803534ec73e839637182bece7df
yakeen15 <shajiratulyakeen@gmail.com>
2024-05-22 21:40:08 +0600

    need to fix eval_mul

--- calc.py
+++ calc.py
@@ -1,5 +1,36 @@
+PREC = 2 #precision
+
 def eval_mul(string):
-    pass
+    string = list(string)
+    ops = ['*', '/']
+    fullops = ['+', '-', '*', '/']
+    num1 = 0
+    num2 = 0
+    index = 0
+    while index < len(string):
+        char = string[index]
+        if char in ops:
+            i = index
+            while i>-1:
+                if string[i] in fullops or i==0:
+                    break
+                i = i-1
+            num1 = float(str(string[i:index]))
+            j = index
+            while j<len(string):
+                if string[j] in fullops or j==len(string)-1:
+                    break
+                j = j+1
+            num2 = float(str(string[index+1:j]))
+            if char == ops[0]:
+                result = num1*num2
+            else:
+                result = num1/num2
+            string[i:j] = str(round(result, PREC))
+        index = index + 1
+    return str(string)
+
+        
 
 def op_adjacent(string):
     slist = list(string)
@@ -50,12 +81,16 @@ def eval_expr(ops, nums):
         if len(nums)>1:
             del nums[0]
             nums[0] = str(result)
-    return (-1)**negcount*(float(nums[0]))
+    return (-1)**negcount*(round(float(nums[0]), PREC))
 
-for i in range(100):
-    inputstring = input("Enter expression: ")
-    if inputstring == 'x':
-        break
-    mainstring = '0'+op_adjacent(inputstring)
-    ops, nums = separate_expression(mainstring)
-    print(eval_expr(ops, nums))
+
+inputstring = input("Enter expression: ")
+print(eval_mul(inputstring))
+
+#for i in range(100):
+#    inputstring = input("Enter expression: ")
+#    if inputstring == 'x':
+#        break
+#    mainstring = '0'+op_adjacent(inputstring)
+#    ops, nums = separate_expression(mainstring)
+#    print(eval_expr(ops, nums))
